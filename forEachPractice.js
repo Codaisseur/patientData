49e43125b2793deba07464c4aa7333e9970ec378
Reinoptland <reinoptland@gmail.com>
2020-02-16 15:09:17 -0800

    forEach examples

---
+++ forEachPractice.js
@@ -0,0 +1,24 @@
+const patients = require("./patients");
+
+// declare function
+function displayFullName(patient) {
+  // HERE IS HOW TO CHECK YOURSELF
+  // console.log('CHECK TO SEE WHAT IS PATIENT:', patient)
+  const { firstName, lastName } = patient;
+
+  const fullName = `${firstName} ${lastName}`;
+
+  console.log(fullName);
+}
+
+// pass as callback
+patients.forEach(displayFullName);
+
+// pass anonymous function directly
+patients.forEach(function(patient) {
+  // HERE IS HOW TO CHECK YOURSELF
+  // console.log("CHECK TO SEE WHAT IS PATIENT:", patient);
+  const { firstName, age } = patient;
+
+  console.log(`${firstName} - age: ${age}`);
+});
