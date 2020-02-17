b1c7b3cc15562cdb59f57634bdb253c860228fce
Reinoptland <reinoptland@gmail.com>
2020-02-16 19:32:00 -0800

    Handle patient not being found

--- patientByEmail.js
+++ patientByEmail.js
@@ -1,21 +1,13 @@
 // patientByEmail.js
 const patients = require("./patients");
-// Dynamic input: run with $ node patientByEmail.js zrothery8@yellowpages.com
 const email = process.argv[2];
 
-console.log(patients);
-
 const specificPatient = patients.find(function(patient) {
-  console.log(
-    "EMAIL of patient:",
-    patient.email,
-    "Email we are looking for:",
-    email,
-    "match?",
-    patient.email === email
-  );
-
   return patient.email === email;
 });
 
-console.log(specificPatient);
+if (specificPatient !== undefined) {
+  console.log("OUTPUT:", specificPatient);
+} else {
+  console.log(`Patient with email: ${email}, not found`);
+}
