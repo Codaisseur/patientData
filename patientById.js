071b5e5c982af7ce2215a092b2b1c3b6d72b71bf
Reinoptland <reinoptland@gmail.com>
2020-02-16 17:38:40 -0800

    Give feedback when patient is not found

--- patientById.js
+++ patientById.js
@@ -8,4 +8,8 @@ const specificPatient = patients.find(function(patient) {
   return id === patient.id;
 });
 
-console.log("OUTPUT:", specificPatient);
+if (specificPatient !== undefined) {
+  console.log("OUTPUT:", specificPatient);
+} else {
+  console.log(`Patient with id: ${id}, not found`);
+}
