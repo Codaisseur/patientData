edfbb67b5ce2749ad15384121acf502c1286e8c5
Reinoptland <reinoptland@gmail.com>
2020-02-16 19:38:52 -0800

    Cleanup and handle patient not being found

--- patientByPhoneNumber.js
+++ patientByPhoneNumber.js
@@ -1,17 +1,12 @@
 const patients = require("./patients");
 const phoneNumber = process.argv[2];
 
-console.log(patients);
-
 const specificPatient = patients.find(function(patient) {
-  console.log(
-    "WHAT IS PATIENT?",
-    patient.phoneNumber,
-    "Match?",
-    patient.phoneNumber === phoneNumber
-  );
-
   return patient.phoneNumber === phoneNumber;
 });
 
-console.log(specificPatient);
+if (specificPatient !== undefined) {
+  console.log(specificPatient);
+} else {
+  console.log(`Patient with phone number: ${phoneNumber}, not found`);
+}
