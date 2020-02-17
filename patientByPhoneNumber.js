7ae1da7f114484ef60467a34f63865d3f814b3a1
Reinoptland <reinoptland@gmail.com>
2020-02-16 19:36:17 -0800

    Check logic with console.log

--- patientByPhoneNumber.js
+++ patientByPhoneNumber.js
@@ -4,7 +4,12 @@ const phoneNumber = "+86-511-277-5074";
 console.log(patients);
 
 const specificPatient = patients.find(function(patient) {
-  console.log("WHAT IS PATIENT?", patient);
+  console.log(
+    "WHAT IS PATIENT?",
+    patient.phoneNumber,
+    "Match?",
+    patient.phoneNumber === phoneNumber
+  );
 });
 
 console.log(specificPatient);
