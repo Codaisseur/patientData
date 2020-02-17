36db71a376cbde2da83fac8cf2e6e899f6c0205e
Reinoptland <reinoptland@gmail.com>
2020-02-16 19:27:05 -0800

    console.log to write the logic

--- patientByEmail.js
+++ patientByEmail.js
@@ -5,7 +5,14 @@ const email = "radolthineg@mayoclinic.com";
 console.log(patients);
 
 const specificPatient = patients.find(function(patient) {
-  console.log(patient);
+  console.log(
+    "EMAIL of patient:",
+    patient.email,
+    "Email we are looking for:",
+    email,
+    "match?",
+    patient.email === email
+  );
 });
 
 console.log(specificPatient);
